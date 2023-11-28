02d361dd360240a629f2dca3052c6426dc3202f3
NicolasEstebanCuervo <rojascuervo942@gmail.com>
2023-11-27 20:49:29 -0500

    Animation correction

--- src/Componentes/Animacion_Reloj.jsx
+++ src/Componentes/Animacion_Reloj.jsx
@@ -21,14 +21,18 @@ const AnimacionReloj = ({ animate, timer, children }) => {
 };
 
 const calculateSize = () => {
-  const smallScreenSize = 90;
+  const extraSmallScreenSize = 60;
+  const smallScreenSize = 110;
   const mediumScreenSize = 120;
   const largeScreenSize = 150;
   const extraLargeScreenSize = 200;
 
   const screenSize = window.innerWidth;
 
-  if (screenSize <= 200) {
+  if (screenSize <= 150) {
+    return extraSmallScreenSize;
+  } 
+  else if (screenSize <= 200) {
     return smallScreenSize;
   } else if (screenSize <= 300) {
     return mediumScreenSize;
